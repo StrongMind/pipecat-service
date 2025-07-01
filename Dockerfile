b36f97c4d1406b6bd576f01cf16baea41f5503ee
Paul Shippy <paul.shippy@strongmind.com>
2025-07-01 16:50:46 -0700

    Upgrade to python 3.12

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,14 @@
-FROM dailyco/pipecat-base:latest
+FROM python:3.12-slim
+WORKDIR /app
+RUN apt update && apt install -y libopenblas-dev libresample1 libresample-dev && apt clean
+COPY ./base_requirements.txt /app/base_requirements.txt
+RUN pip install --no-cache-dir --upgrade -r /app/base_requirements.txt
+COPY ./app.py ./waiting_server.py /app/
+ARG VERSION
+ENV IMAGE_VERSION=$VERSION
+CMD ["sh", "-c", "if [ -f /app/pre-app.sh ]; then sh /app/pre-app.sh; fi && exec python app.py"]
+
+RUN python --version
 
 COPY ./requirements.txt requirements.txt
 
---
+++ app.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+import json
+import sys
+from os import environ
+from typing import Annotated
+
+from bot import bot
+from fastapi import FastAPI, Header, WebSocket
+from fastapi.websockets import WebSocketState
+from loguru import logger
+from pipecatcloud.agent import (
+    DailySessionArguments,
+    PipecatSessionArguments,
+    SessionArguments,
+    WebSocketSessionArguments,
+)
+
+from waiting_server import Config, WaitingServer
+
+app = FastAPI()
+server_config = Config(
+    environ.get("SHUTDOWN_TIMEOUT", 7200),
+    app,
+    host="0.0.0.0",
+    port=int(environ.get("PORT", 8080)),
+)
+server = WaitingServer(server_config)
+
+session_logger_format = (
+    "{time:YYYY-MM-DD HH:mm:ss.SSS} | "
+    "{level: <8} | "
+    "{name}:{function}:{line} | "
+    "{extra[session_id]} - {message}"
+)
+logger.remove()
+logger.add(sys.stderr, format=session_logger_format)
+logger.configure(extra={"session_id": "NONE"})
+
+image_version = environ.get("IMAGE_VERSION", "unknown")
+
+
+async def run_bot(args: SessionArguments):
+    metadata = {
+        "session_id": args.session_id,
+        "image_version": image_version,
+    }
+
+    with logger.contextualize(session_id=args.session_id):
+        logger.info(f"Starting bot session with metadata: {json.dumps(metadata)}")
+        try:
+            await bot(args)
+        except Exception as e:
+            logger.error(f"Exception running bot(): {e}")
+
+
+@app.post("/bot")
+async def handle_bot_request(
+    body: dict,
+    x_daily_room_url: Annotated[str | None, Header()] = None,
+    x_daily_room_token: Annotated[str | None, Header()] = None,
+    x_daily_session_id: Annotated[str | None, Header()] = None,
+):
+    if x_daily_room_url and x_daily_room_token:
+        args = DailySessionArguments(
+            session_id=x_daily_session_id,
+            room_url=x_daily_room_url,
+            token=x_daily_room_token,
+            body=body,
+        )
+    else:
+        args = PipecatSessionArguments(
+            session_id=x_daily_session_id,
+            body=body,
+        )
+
+    await run_bot(args)
+
+    return {}
+
+
+@app.websocket("/ws")
+async def handle_websocket(
+    ws: WebSocket, x_daily_session_id: Annotated[str | None, Header()] = None
+):
+    await ws.accept()
+
+    args = WebSocketSessionArguments(
+        session_id=x_daily_session_id,
+        websocket=ws,
+    )
+
+    await run_bot(args)
+
+    if ws.state == WebSocketState.CONNECTED:
+        await ws.close()
+
+
+if __name__ == "__main__":
+    try:
+        server.run()
+    except KeyboardInterrupt:
+        pass
